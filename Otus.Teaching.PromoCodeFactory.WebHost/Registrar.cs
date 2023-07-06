c8548abda5a4df797185774ff7ee0923962ad09f
Дмитрий Брилько <DimaBrilko@gmail.com>
2023-07-06 15:41:05 +0300

    Теперь названия приватных методов будут устанавливать, а не довбавлять сервисы.

--- Otus.Teaching.PromoCodeFactory.WebHost/Registrar.cs
+++ Otus.Teaching.PromoCodeFactory.WebHost/Registrar.cs
@@ -11,14 +11,20 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
         public static IServiceCollection AddCustomServicesExtensionMethod(
             this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddSqLiteDB(configuration);
+            services.InstallSqLiteDB(configuration);
             return services;
         }
 
-        private static IServiceCollection AddSqLiteDB(this IServiceCollection services, IConfiguration configuration)
+        private static IServiceCollection InstallSqLiteDB(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<ApplicationContext>(options => options.UseSqlite(connectionString));
+            return services;
+        }
+
+        private static IServiceCollection InstallRepositories(this IServiceCollection services)
+        {
+            
             return services;
         }
     }
