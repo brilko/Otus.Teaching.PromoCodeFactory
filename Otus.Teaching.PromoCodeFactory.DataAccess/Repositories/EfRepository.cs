f6da22a612a938bd67a19cba210d49fa96544d53
Дмитрий Брилько <DimaBrilko@gmail.com>
2023-07-06 14:24:16 +0300

    Реализован интерфейс EFRepository.

--- Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
+++ Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -1,7 +1,11 @@
-﻿using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
 using Otus.Teaching.PromoCodeFactory.Core.Domain;
+using Otus.Teaching.PromoCodeFactory.DataAccess.DataBaseContext;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,34 +15,70 @@ namespace Otus.Teaching.PromoCodeFactory.DataAccess.Repositories
         : IRepository<T>
         where T : BaseEntity
     {
-        public Task<bool> DeleteAsync(Guid id)
+
+        protected readonly DbSet<T> entitySet;
+        protected readonly ApplicationContext context;
+
+        public EfRepository(ApplicationContext context)
         {
-            throw new NotImplementedException();
+            this.context = context;
+            entitySet = context.Set<T>();
         }
 
-        public Task<IEnumerable<T>> GetAllAsync()
+        public async Task<bool> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await Task.Run(() => {
+                if (!IsExist(id, out T ent)) {
+                    return false;
+                }
+                entitySet.Remove(ent);
+                context.SaveChanges();
+                return true;
+            });
         }
 
-        public Task<T> GetByIdAsync(Guid id)
+        public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await entitySet.ToListAsync();
         }
 
-        public Task<bool> IsExistAsync(Guid id)
+        public async Task<T> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await entitySet.FindAsync(id);
         }
 
-        public Task<Guid> PostAsync(T entity)
+
+        private bool IsExist(Guid id, out T entity) 
         {
-            throw new NotImplementedException();
+            entity = entitySet.Find(id);
+            return entity != null;
+        }
+        public async Task<bool> IsExistAsync(Guid id)
+        {
+            return await Task.Run(() => {
+                return IsExist(id, out _);
+            });
         }
 
-        public Task<bool> UpdateAsync(T entity)
+        public async Task<Guid> PostAsync(T entity)
         {
-            throw new NotImplementedException();
+            return await Task.Run(() => {
+                var entToReturn = entitySet.Add(entity);
+                context.SaveChanges();
+                return entToReturn.Entity.Id;
+            });
+        }
+
+        public async Task<bool> UpdateAsync(T entity)
+        {
+            return await Task.Run(() => {
+                if (!IsExist(entity.Id, out _)) {
+                    return false;
+                }
+                entitySet.Update(entity);
+                context.SaveChanges();
+                return true;
+            });
         }
     }
 }
